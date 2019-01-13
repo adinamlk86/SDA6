a4c5f95c7c912efe02e08325029000d71c469d5b
adinamlk86 <adina.dumitrescu1@gmail.com>
2019-01-13 14:27:25 +0200

    upload

---
+++ src/main/java/arrayoperations/ArraySort.java
@@ -0,0 +1,56 @@
+package arrayoperations;
+
+public class ArraySort {
+
+
+    static void sortListNumbers(int[] listToBeOrdered){
+
+        for (int i = 0; i < listToBeOrdered.length; i++) {
+            for (int j = 0; j < listToBeOrdered.length; j++) {
+                if (listToBeOrdered[i] < listToBeOrdered[j]) {
+                    int tempElement = listToBeOrdered[i];
+                    listToBeOrdered[i] = listToBeOrdered[j];
+                    listToBeOrdered[j] = tempElement;
+                }
+            }
+        }
+        System.out.println("Ordered number list: ");
+
+        for(int i=0;i<listToBeOrdered.length;i++){
+            System.out.print(listToBeOrdered[i]+" ");
+        }
+    }
+
+    static void sortListString(String[] listToBeOrdered){
+
+        for (int i = 0; i < listToBeOrdered.length; i++) {
+            for (int j = i+1; j < listToBeOrdered.length; j++) {
+                if (listToBeOrdered[i].compareTo(listToBeOrdered[j])>0) {
+                    String tempElement = listToBeOrdered[i];
+                    listToBeOrdered[i] = listToBeOrdered[j];
+                    listToBeOrdered[j] = tempElement;
+                }
+            }
+        }
+        System.out.println("Ordered string list: ");
+
+        for(int i=0;i<listToBeOrdered.length;i++){
+            System.out.print(listToBeOrdered[i]+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[]listNumbers = {9,3,8,2,7,5,6,4,1};
+        String[] listString = {"g","a","h","e","b","d","c","f"};
+        sortListNumbers(listNumbers);
+        System.out.println();
+        sortListString(listString);
+
+
+
+
+
+
+    }
+}
